5c2c0ed5d481d58a618997344383b856be704d9d
Nagi70 <n.yoshimura.060@ms.saitama-u.ac.jp>
2025-10-21 17:37:14 +0900

    add: *.o files to compare binary size

---
+++ tr_uart_led_sample2/tecsrust_uart_led_sample/target/debug/build/itron-48cbc1a0912f2130/build_script_build-48cbc1a0912f2130.d
@@ -0,0 +1,5 @@
+/home/yosshy/research/FMP3-TECS/tr_uart_led_sample2/tecsrust_uart_led_sample/target/debug/build/itron-48cbc1a0912f2130/build_script_build-48cbc1a0912f2130.d: /home/yosshy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itron-0.1.9/build.rs
+
+/home/yosshy/research/FMP3-TECS/tr_uart_led_sample2/tecsrust_uart_led_sample/target/debug/build/itron-48cbc1a0912f2130/build_script_build-48cbc1a0912f2130: /home/yosshy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itron-0.1.9/build.rs
+
+/home/yosshy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itron-0.1.9/build.rs:
